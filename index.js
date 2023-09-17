2c2266256d5dc26712aaefea379789e152861c15
Lexi Geller <leximoconner@yahoo.com>
2023-09-16 22:48:23 -0400

    added code for prompting user input

--- index.js
+++ index.js
@@ -5,17 +5,59 @@ const { Triangle, Circle, Square } = require('./lib/shapes');
 async function main() {
   try {
     const userInput = await inquirer.prompt([
-      // Define Inquirer prompts here based on the README's requirements
+      {
+        type: 'input',
+        name: 'logoText',
+        message: 'Enter up to three characters for the logo text:',
+        validate: function (input) {
+          return input.length <= 3 ? true : 'Please enter up to three characters.';
+        },
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color keyword or hexadecimal number for the text color:',
+        validate: function (input) {
+          // You can add more validation here based on color formats
+          return /^[a-fA-F0-9]{6}$/.test(input) || /[a-z]+/i.test(input) ? true : 'Invalid color format.';
+        },
+      },
+      {
+        type: 'list',
+        name: 'shapeChoice',
+        message: 'Choose a shape for the logo:',
+        choices: ['circle', 'triangle', 'square'],
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color keyword or hexadecimal number for the shape color:',
+        validate: function (input) {
+          // You can add more validation here based on color formats
+          return /^[a-fA-F0-9]{6}$/.test(input) || /[a-z]+/i.test(input) ? true : 'Invalid color format.';
+        },
+      },
     ]);
 
-    // Create an instance of the selected shape (e.g., Triangle, Circle, Square)
-    const shape = new SelectedShape();
+    // Create an instance of the selected shape class
+    let selectedShape;
+    switch (userInput.shapeChoice) {
+      case 'circle':
+        selectedShape = new Circle();
+        break;
+      case 'triangle':
+        selectedShape = new Triangle();
+        break;
+      case 'square':
+        selectedShape = new Square();
+        break;
+    }
 
     // Set the shape's color based on user input
-    shape.setColor(userInput.shapeColor);
+    selectedShape.setColor(userInput.shapeColor);
 
     // Generate the SVG string for the shape
-    const svg = shape.render();
+    const svg = selectedShape.render();
 
     // Save the SVG to a file named 'logo.svg'
     fs.writeFileSync('logo.svg', svg);
